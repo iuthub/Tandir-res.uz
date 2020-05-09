3a67f70d12c3a8b42af587c01714c8d39ea11465
My Name <murodbek99.murodov@gmail.com>
2020-05-09 21:44:59 +0500

    new changes

--- resources/views/admin/orders/orders.blade.php
+++ resources/views/admin/orders/orders.blade.php
@@ -6,12 +6,13 @@
 
 <h1>Orders</h1>
 
-<div class="container" style="width: 100%;    margin-left: -26px;">
+<div class="container" style="width: 100%;">
 
     <table class="table table-hover table-bordered table-responsive">
       <thead>
         <tr>
           {{-- <th>ID</th> --}}
+          <th>ID </th>
           <th>First Name </th>
           <th>Email</th>
           <th>Phone Number</th>
@@ -20,9 +21,7 @@
           <th>Order Time</th>
           <th>Total Price</th>
           <th>Is Approved</th>
-          <th>Approve</th>
-          <th>Disapprove</th>
-          <th>Delete</th>
+          <th>Edit</th>
         </tr>
       </thead>
       <tbody>
@@ -32,6 +31,7 @@
 
             <tr>
                 {{-- <td>{{ $order->id }}</td> --}}
+                <td>{{ $order->id }}</td>
                 <td>{{ $order->first_name }}</td>
                 <td>{{ $order->email }}</td>
                 <td>{{ $order->phone_number }}</td>
@@ -40,15 +40,43 @@
                 <td>{{ $order->time }}</td>
                 <td>{{ $order->total_price }}</td>
 
-                @if ($order->is_approved==1)
+                <td>
+                    @if ($order->is_approved==1)
+
+                        {!! Form::open(['method'=>'PATCH','action'=>['AdminOrdersController@update',$order->id]]) !!}
+
+                        <input type="hidden" name="is_active" value="0">
+
+                            <div class="form-group">
+                                {!! Form::submit('Unapprove', ['class'=>'btn btn-danger']) !!}
+                            </div>
+                            {!! Form::close() !!}
+                    @else
+
+                    {!! Form::open(['method'=>'PATCH','action'=>['AdminOrdersController@update',$order->id]]) !!}
+
+                        <input type="hidden" name="is_active" value="1">
+
+                            <div class="form-group">
+                                {!! Form::submit('Approve', ['class'=>'btn btn-primary']) !!}
+                            </div>
+                            {!! Form::close() !!}
+
+
+                    @endif
+
+
+                </td>
+
+                {{-- @if ($order->is_approved==1)
                     <td> Approved</td>
                 @else
                     <td> Disapproved</td>
                 @endif
 
                 <td><a href="#" class="btn btn-primary">Approve</a></td>
-                <td><a href="#" class="btn btn-danger">Disapprove</a></td>
-                <td><a href="#" class="btn btn-danger">Delete</a></td>
+                <td><a href="#" class="btn btn-danger">Disapprove</a></td> --}}
+                <td><a href="#" class="btn btn-primary">Edit</a></td>
             </tr>
         @endforeach
     @endif
